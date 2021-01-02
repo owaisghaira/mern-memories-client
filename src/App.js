e0aa898c3ef11ae79a66acc5e3aa3c62e1d6466d
owaisghaira <owais.ghaira99@gmail.com>
2021-01-02 12:50:33 +0500

    redux boiler-plate added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, AppBar, Typography, Grid, Grow } from '@material-ui/core'
+import memories from './images/memories.png'
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+import useStyles from './styles'
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container maxWidth='lg'>
+        <AppBar className={classes.appBar} position='static' color='inherit'>
+          <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
+          <img className={classes.image} src={memories} alt='memories' width='60' />
+
+        </AppBar>
+        <Grow in>
+          <Container>
+            <Grid container justify='space-between' alignItems='stretch' spacing={4}>
+              <Grid item xs={12} md={7}>
+                <Posts />
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <Form />
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+      </Container>
     </div>
   );
 }
---
+++ src/api/index.js
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+const url = 'https://localhost:5000/posts'
+
+export const fetchData = () => axios.get();
